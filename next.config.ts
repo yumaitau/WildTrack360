9e8a5e1accd687194edc4d0c10965a71b20f8568
Justin Middler <justin@yumait.com.au>
2026-02-25 22:41:50 +1100

    cant use these type of optimisations

--- next.config.ts
+++ next.config.ts
@@ -12,13 +12,6 @@ const nextConfig: NextConfig = {
       },
     ],
   },
-  // Skip linting and type-checking during build (run these in CI instead)
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  typescript: {
-    ignoreBuildErrors: true,
-  },
 };
 
 export default withSentryConfig(nextConfig, {
@@ -31,11 +24,6 @@ export default withSentryConfig(nextConfig, {
 
   // Only print logs for uploading source maps in CI
   silent: !process.env.CI,
-
-  // Disable source map upload on VPS builds to save CPU/memory
-  sourcemaps: {
-    disable: !process.env.CI,
-  },
 
   // For all available options, see:
   // https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/
