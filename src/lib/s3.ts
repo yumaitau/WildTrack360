5dbd8d8d07a4f348357015ea208f5b8119688344
Justin Middler <justin@yumait.com.au>
2026-03-11 13:10:56 +1100

    fix s3 bucket bug

--- src/lib/s3.ts
+++ src/lib/s3.ts
@@ -1,21 +1,11 @@
 import { S3Client, PutObjectCommand, GetObjectCommand, DeleteObjectCommand } from '@aws-sdk/client-s3'
 
-const region = process.env.S3_REGION || 'us-east-1'
-
-// Wasabi endpoint: us-east-1 uses s3.wasabisys.com, others use s3.{region}.wasabisys.com
-const endpoint =
-  region === 'us-east-1'
-    ? 'https://s3.wasabisys.com'
-    : `https://s3.${region}.wasabisys.com`
-
 const s3Client = new S3Client({
-  endpoint,
-  region,
+  region: process.env.S3_REGION || 'us-east-1',
   credentials: {
     accessKeyId: process.env.S3_ACCESS_KEY_ID!,
     secretAccessKey: process.env.S3_SECRET_ACCESS_KEY!,
   },
-  forcePathStyle: true,
 })
 
 const BUCKET_NAME = process.env.S3_BUCKET_NAME || 'wildtrack360'
